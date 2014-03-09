795c9d886207418c41c2e961dcb48fa5fdc1e9eb
Linus Metzler <linus.metzler@gmail.com>
2014-03-09 09:34:58 +0100

    ordering results

--- get.php
+++ get.php
@@ -2,7 +2,7 @@
 require_once 'db.php';
 $taList = array();
 $subjectList = array();
-$entries = ORM::for_table('exercises')->find_many();
+$entries = ORM::for_table('exercises')->order_by_desc('ex_set')->find_many();
 foreach ($entries as $exercise) {
 	$taList[] = $exercise->ta;
 	$subjectList[] = $exercise->subject;
